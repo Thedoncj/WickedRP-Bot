4cbfbc82e35e518500ad8a21a97635bc7322ba64
Thedoncj <tyseanhall13@gmail.com>
2025-06-19 11:35:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -554,6 +554,5 @@ app.get("/", (req, res) => {
   res.status(200).send("Bot is running!");
 });
 
-# --- Run the bot ---
-if __name__ == "__main__":
-    bot.run(os.getenv("DISCORD_BOT_TOKEN"))
+from keep_alive import keep_alive
+keep_alive()
